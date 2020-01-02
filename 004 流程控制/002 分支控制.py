80eaaba3f9e04ecb34a1c0ee86f0f98f8a90c40f
ccpwcn <ccpwcn@gmail.com>
2020-01-02 13:44:05 +0800

    流程控制

---
+++ 004 流程控制/002 分支控制.py
@@ -0,0 +1,8 @@
+print('start')
+a = 3
+b = 3
+if a is b:
+    print('yes')
+else:
+    print('no')
+print('end')
---
+++ 004 流程控制/003 循环控制.py
@@ -0,0 +1,16 @@
+# 简单循环
+for i in range(10):
+    print(i)
+
+# 设置循环范围
+for i in range(5, 10):
+    print(i)
+
+# 设置循环步进
+for i in range(1, 10, 2):
+    print(i)
+
+# 死循环
+while True:
+    print(1)
+
