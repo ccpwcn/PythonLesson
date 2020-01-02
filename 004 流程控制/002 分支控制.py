d53671988b9ddb05f5a85706cf19caa2b06949e9
ccpwcn <ccpwcn@gmail.com>
2020-01-02 14:21:22 +0800

    流程控制

--- 004 流程控制/002 分支控制.py
+++ 004 流程控制/002 分支控制.py
@@ -6,3 +6,13 @@ if a is b:
 else:
     print('no')
 print('end')
+
+score = 10
+if score < 60:
+    print('不及格')
+elif score < 90:
+    print('良好')
+elif score < 100:
+    print('优秀')
+else:
+    print('分数错误')
